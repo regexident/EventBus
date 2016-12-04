4fe39f1899609e29dff907483ff9987bdfc7e21f
Vincent Esche <regexident@gmail.com>
2016-12-04 18:24:49 +0100

    Initial Commit

---
+++ EventBus/EventBus.h
@@ -0,0 +1,19 @@
+//
+//  EventBus.h
+//  EventBus
+//
+//  Created by Vincent Esche on 04/12/2016.
+//  Copyright © 2016 Vincent Esche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventBus.
+FOUNDATION_EXPORT double EventBusVersionNumber;
+
+//! Project version string for EventBus.
+FOUNDATION_EXPORT const unsigned char EventBusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventBus/PublicHeader.h>
+
+
