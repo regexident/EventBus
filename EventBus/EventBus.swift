295c4caf75b6492888a01acb01880c430f7d64c5
Vincent Esche <regexident@gmail.com>
2018-01-22 12:30:37 +0100

    Replaced `strict` boolean flag with extended option set

--- EventBus/EventBus.swift
+++ EventBus/EventBus.swift
@@ -29,20 +29,35 @@ public protocol EventNotifiable: class {
     func notify<T>(_ eventType: T.Type, closure: @escaping (T) -> ())
 }
 
+public struct Options: OptionSet {
+
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    public static let warnUnknown = Options(rawValue: 1 << 0)
+
+    public static let warnDropped = Options(rawValue: 1 << 1)
+
+    public static let logEvents = Options(rawValue: 1 << 2)
+}
+
 public class EventBus {
 
-    struct WeakBox : Hashable {
-        weak var inner: AnyObject?
+    internal struct WeakBox: Hashable {
+        internal weak var inner: AnyObject?
 
-        init(_ inner: AnyObject) {
+        internal init(_ inner: AnyObject) {
             self.inner = inner
         }
 
-        static func == (lhs: WeakBox, rhs: WeakBox) -> Bool {
+        internal static func == (lhs: WeakBox, rhs: WeakBox) -> Bool {
             return lhs.inner === rhs.inner
         }
 
-        var hashValue: Int {
+        internal var hashValue: Int {
             guard let inner = self.inner else {
                 return 0
             }
@@ -52,16 +67,27 @@ public class EventBus {
 
     public static let shared: EventBus = EventBus()
 
-    fileprivate var registered: [ObjectIdentifier: String]?
+    public let options: Options
+
+    fileprivate var registered: [ObjectIdentifier: String] = [:]
     fileprivate var subscribed: [ObjectIdentifier: Set<WeakBox>] = [:]
     fileprivate var chained: Set<WeakBox> = []
 
     fileprivate let serialQueue: DispatchQueue = DispatchQueue(label: "com.regexident.eventbus")
     fileprivate let queue: DispatchQueue
 
-    public init(strict: Bool = false, queue: DispatchQueue = .global()) {
-        self.registered = strict ? [:] : nil
+    private var nameAndAddress: String {
+        var mutableSelf = self
+        return Swift.withUnsafePointer(to: &mutableSelf) { pointer in
+            let name = String(describing: type(of: self))
+            let address = String(format: "%p", pointer)
+            return "<\(name): \(address)>"
+        }
+    }
+
+    public init(options: Options? = nil, queue: DispatchQueue = .global()) {
         self.queue = queue
+        self.options = options ?? Options()
     }
 
     fileprivate func validateSubscriber<T>(subscriber: T) {
@@ -71,60 +97,87 @@ public class EventBus {
             #if DEBUG
                 fatalError(message)
             #else
-                print("Warning:", message)
+                print(message)
             #endif
         }
     }
 
-    fileprivate func validateEventType<T>(type eventType: T.Type) {
+    fileprivate func warnIfUnknown<T>(_ eventType: T.Type) {
         let identifier = ObjectIdentifier(eventType)
-        guard let registered = self.registered else {
+        guard self.options.contains(.warnUnknown) else {
             return
         }
-        if registered[identifier] == nil {
-            let names = Array(registered.values).joined(separator: ", ")
-            let message = "Expected registered event type (e.g. \(names)), found: \(eventType)"
-            #if DEBUG
-                fatalError(message)
-            #else
-                print("Warning:", message)
-            #endif
+        guard self.registered[identifier] != nil else {
+            return
         }
+        let names = Array(self.registered.values).joined(separator: ", ")
+        let message = "\(self.nameAndAddress): Expected event of registered type (e.g. \(names)), found: \(eventType)"
+        #if DEBUG
+            fatalError(message)
+        #else
+            print(message)
+        #endif
+    }
+
+    fileprivate func warnIfDropped<T>(_ eventType: T.Type) {
+        let identifier = ObjectIdentifier(eventType)
+        guard self.options.contains(.warnDropped) else {
+            return
+        }
+        guard self.subscribed[identifier] == nil else {
+            return
+        }
+        let message = "\(self.nameAndAddress): Event of type '\(eventType)' was not handled."
+        #if DEBUG
+            fatalError(message)
+        #else
+            print(message)
+        #endif
+    }
+
+    fileprivate func logEvent<T>(_ eventType: T.Type) {
+        #if DEBUG
+            if self.options.contains(.logEvents) {
+                print("\(self.nameAndAddress): Received event '\(eventType)'")
+            }
+        #endif
+    }
+
+    fileprivate func pruned<T>(subscribed: Set<WeakBox>, for eventType: T.Type) -> Set<WeakBox>? {
+        let filtered = subscribed.filter { $0.inner is T }
+        return filtered.isEmpty ? nil : Set(filtered)
     }
 }
 
 extension EventBus: EventRegistrable {
     public func register<T>(forEvent eventType: T.Type) {
         let identifier = ObjectIdentifier(eventType)
-        if self.registered == nil {
-            self.registered = [:]
-        }
-        self.registered![identifier] = String(describing: eventType)
+        self.registered[identifier] = String(describing: eventType)
     }
 }
 
 extension EventBus: EventSubscribable {
     public func add<T>(subscriber: T, for eventType: T.Type) {
         self.validateSubscriber(subscriber: subscriber)
-        self.validateEventType(type: eventType)
+        self.warnIfUnknown(eventType)
         self.serialQueue.sync {
             let identifier = ObjectIdentifier(eventType)
             var subscribed = self.subscribed[identifier] ?? []
             let weakBox = WeakBox(subscriber as AnyObject)
             subscribed.insert(weakBox)
-            self.subscribed[identifier] = Set(subscribed.filter { $0.inner is T })
+            self.subscribed[identifier] = self.pruned(subscribed: subscribed, for: eventType)
         }
     }
 
     public func remove<T>(subscriber: T, for eventType: T.Type) {
         self.validateSubscriber(subscriber: subscriber)
-        self.validateEventType(type: eventType)
+        self.warnIfUnknown(eventType)
         self.serialQueue.sync {
             let identifier = ObjectIdentifier(eventType)
             var subscribed = self.subscribed[identifier] ?? []
             let weakBox = WeakBox(subscriber as AnyObject)
             let _ = subscribed.remove(weakBox)
-            self.subscribed[identifier] = Set(subscribed.filter { $0.inner is T })
+            self.subscribed[identifier] = self.pruned(subscribed: subscribed, for: eventType)
         }
     }
 
@@ -134,7 +187,7 @@ extension EventBus: EventSubscribable {
             for (identifier, var subscribed) in self.subscribed {
                 let weakBox = WeakBox(subscriber as AnyObject)
                 let _ = subscribed.remove(weakBox)
-                self.subscribed[identifier] = Set(subscribed.filter { $0.inner is T })
+                self.subscribed[identifier] = subscribed
             }
         }
     }
@@ -148,7 +201,7 @@ extension EventBus: EventSubscribable {
     internal func has<T>(subscriber: T, for eventType: T.Type) -> Bool {
         var result: Bool = false
         self.validateSubscriber(subscriber: subscriber)
-        self.validateEventType(type: eventType)
+        self.warnIfUnknown(eventType)
         self.serialQueue.sync {
             let identifier = ObjectIdentifier(eventType)
             guard let subscribed = self.subscribed[identifier] else {
@@ -164,11 +217,13 @@ extension EventBus: EventSubscribable {
 
 extension EventBus: EventNotifiable {
     public func notify<T>(_ eventType: T.Type, closure: @escaping (T) -> ()) {
-        self.validateEventType(type: eventType)
+        self.warnIfUnknown(eventType)
+        self.warnIfDropped(eventType)
+        self.logEvent(eventType)
         self.serialQueue.sync {
             let identifier = ObjectIdentifier(eventType)
             defer {
-                for eventBus in chained.flatMap({ $0.inner as? EventNotifiable }) {
+                for eventBus in self.chained.flatMap({ $0.inner as? EventNotifiable }) {
                     self.queue.async {
                         eventBus.notify(eventType, closure: closure)
                     }
